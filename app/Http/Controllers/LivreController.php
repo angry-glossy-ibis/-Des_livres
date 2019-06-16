4f1aceda319903b705275c9f5f6aaadc6993b7d1
Gossteer <GossteerPlus@gmail.com>
2019-06-16 04:21:15 +0300

    'Lol3'

---
+++ app/Http/Controllers/LivreController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Livre;
+use Illuminate\Http\Request;
+
+class LivreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Livre  $livre
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Livre $livre)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Livre  $livre
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Livre $livre)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Livre  $livre
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Livre $livre)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Livre  $livre
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Livre $livre)
+    {
+        //
+    }
+}
